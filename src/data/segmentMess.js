7ba73186ebc18a5aba977b8a0e4e83c51fe48100
haylee <hayleeheilman@gmail.com>
2021-02-21 21:17:55 -0700

    added object for lesson

---
+++ src/data/segmentMess.js
@@ -0,0 +1,85 @@
+const segment = {
+  totalCount: 3015,
+  segments: [
+    {
+      id: 1,
+      name: 'Child segment',
+      description: '(1, 2, 4)',
+      parentFg: false,
+      segmentFactors: [
+        {
+          segmentFactorValueId: 1,
+          name: '',
+          value: '',
+          description: '',
+        },
+        {
+          segmentFactorValueId: 1,
+          name: '',
+          value: '',
+          description: '',
+        },
+        {
+          segmentFactorValueId: 1,
+          name: '',
+          value: '',
+          description: '',
+        },
+      ],
+    },
+    {
+      id: 1,
+      name: 'Child segment',
+      description: '(1, 2, 4)',
+      parentFg: false,
+      segmentFactors: [
+        {
+          segmentFactorValueId: 1,
+          name: '',
+          value: '',
+          description: '',
+        },
+        {
+          segmentFactorValueId: 1,
+          name: '',
+          value: '',
+          description: '',
+        },
+        {
+          segmentFactorValueId: 1,
+          name: '',
+          value: '',
+          description: '',
+        },
+      ],
+    },
+    {
+      id: 1,
+      name: 'Child segment',
+      description: '(1, 2, 4)',
+      parentFg: false,
+      segmentFactors: [
+        {
+          segmentFactorValueId: 1,
+          name: '',
+          value: '',
+          description: '',
+        },
+        {
+          segmentFactorValueId: 1,
+          name: '',
+          value: '',
+          description: '',
+        },
+        {
+          segmentFactorValueId: 1,
+          name: '',
+          value: '',
+          description: '',
+        },
+      ],
+    },
+  ],
+};
+
+export default segment;
